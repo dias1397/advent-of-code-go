6003d043895727384f9840d96a91cacbc7f27b17
João Dias <joao.dias1397@gmail.com>
2023-07-09 17:42:35 +0100

    Adds exercise solution template

---
+++ TEMPLATE/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+    fmt.Print("--- Day X: TEMPLATE ---\n\n")
+
+    if len(os.Args) > 1 {
+        input, err := os.ReadFile(os.Args[1]) 
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Unable to read file named %s", os.Args[1])
+            os.Exit(-1)
+        }
+
+        fmt.Fprintf(os.Stdout, "Part1 solution: %d\n", Part1(string(input)))
+        fmt.Fprintf(os.Stdout, "Part2 solution: %d\n", Part2(string(input)))
+    } else {
+        fmt.Fprintf(os.Stderr, "Input file not provided")
+    }
+}
+
+func Part1(input string) int {
+
+    return -1 
+}
+
+func Part2(input string) int {
+
+    return -1 
+}
