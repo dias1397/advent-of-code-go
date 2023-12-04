936ab8aeb2d74d0fa8813f23fae8645c1c2b63bb
João Dias <joao.dias1397@gmail.com>
2023-12-04 21:51:51 +0000

    Adds solution for day 01 part 01

--- 2023/01-trebuchet/main.go
+++ 2023/01-trebuchet/main.go
@@ -3,29 +3,55 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-    fmt.Print("--- Day 1: Trebuchet?! ---\n\n")
+	fmt.Print("--- Day 1: Trebuchet?! ---\n\n")
 
-    if len(os.Args) > 1 {
-        input, err := os.ReadFile(os.Args[1]) 
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "Unable to read file named %s", os.Args[1])
-            os.Exit(-1)
-        }
+	if len(os.Args) > 1 {
+		input, err := os.ReadFile(os.Args[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to read file named %s", os.Args[1])
+			os.Exit(-1)
+		}
 
-        fmt.Fprintf(os.Stdout, "Part1 solution: %d\n", Part1(string(input)))
-        fmt.Fprintf(os.Stdout, "Part2 solution: %d\n", Part2(string(input)))
-    } else {
-        fmt.Fprintf(os.Stderr, "Input file not provided")
-    }
+		fmt.Fprintf(os.Stdout, "Part1 solution: %d\n", Part1(string(input)))
+		fmt.Fprintf(os.Stdout, "Part2 solution: %d\n", Part2(string(input)))
+	} else {
+		fmt.Fprintf(os.Stderr, "Input file not provided")
+	}
 }
 
 func Part1(input string) int {
-    return -1
+	input = strings.TrimSuffix(input, "\n")
+	result := 0
+
+	firstDigit := -1
+	secondDigit := -1
+
+	for _, line := range strings.Split(input, "\n") {
+		for _, value := range strings.Split(line, "") {
+			temp, err := strconv.Atoi(value)
+			if err != nil {
+				continue
+			}
+
+			if firstDigit == -1 {
+				firstDigit = temp
+			}
+			secondDigit = temp
+		}
+		result += ((firstDigit * 10) + secondDigit)
+
+		firstDigit = -1
+		secondDigit = -1
+	}
+
+	return result
 }
 
 func Part2(input string) int {
-    return -1 
+	return -1
 }
