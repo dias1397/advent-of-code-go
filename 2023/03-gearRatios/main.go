d35792c0ac927a9ba6af7fceec6a90f26110bfa5
João Dias <joao.dias1397@gmail.com>
2024-03-17 18:48:07 +0000

    feat: adds solution to day 3 part 2 2023

--- 2023/03-gearRatios/main.go
+++ 2023/03-gearRatios/main.go
@@ -116,7 +116,68 @@ func Part1(input string) (result int) {
 	return result
 }
 
-func Part2(input string) int {
+func Part2(input string) (result int) {
+	input = strings.TrimSuffix(input, "\n")
+	matrix := buildMatrix(input)
 
-	return -1
+	diffs := [8][2]int{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+
+	seen := make(map[[2]int]bool)
+
+	for row := 0; row < len(matrix); row++ {
+		for column := 0; column < len(matrix[row]); column++ {
+			if matrix[row][column] == "*" {
+				numbersDetected := []int{}
+
+				for _, diff := range diffs {
+					searchCoords := [2]int{row + diff[0], column + diff[1]}
+
+					if !seen[searchCoords] && isDigit(matrix[searchCoords[0]][searchCoords[1]]) {
+						numbersDetected = append(numbersDetected, getNumber(matrix, searchCoords, seen))
+
+					}
+				}
+
+				if len(numbersDetected) > 1 {
+					sum := 1
+					for _, number := range numbersDetected {
+						sum *= number
+					}
+
+					result += sum
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func getNumber(matrix [][]string, detectedCoords [2]int, seen map[[2]int]bool) int {
+	row, column := detectedCoords[0], detectedCoords[1]
+	for column-1 >= 0 {
+		if isDigit(matrix[row][column-1]) {
+			column--
+		} else {
+			break
+		}
+	}
+
+	number := ""
+	for column < len(matrix[row]) && isDigit(matrix[row][column]) {
+		number += matrix[row][column]
+		seen[[2]int{row, column}] = true
+		column++
+	}
+
+	numberInt, err := strconv.Atoi(number)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to parse %v to int\n", number)
+	}
+
+	return numberInt
 }
